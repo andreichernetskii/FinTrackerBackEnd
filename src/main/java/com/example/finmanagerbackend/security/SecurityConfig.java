e43b9366a1f6bde2904d9797df75636818a4bbf3
orcxus <73879364+andreichernetskii@users.noreply.github.com>
2023-12-27 17:28:01 +0100

    adding comments

--- src/main/java/com/example/finmanagerbackend/security/SecurityConfig.java
+++ src/main/java/com/example/finmanagerbackend/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.finmanagerbackend.security;
 import com.example.finmanagerbackend.application_user.UserDetailsServiceImpl;
 import com.example.finmanagerbackend.jwt.AuthEntryPointJwt;
 import com.example.finmanagerbackend.jwt.AuthTokenFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,15 +11,16 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
+/**
+ * Security configuration class that defines security-related beans and settings.
+ */
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
@@ -32,11 +32,13 @@ public class SecurityConfig {
         this.unauthorizedHandle = unauthorizedHandle;
     }
 
+    // Creates an instance of the AuthTokenFilter bean.
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
     }
 
+    // Creates an instance of the DaoAuthenticationProvider bean.
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -47,16 +49,19 @@ public class SecurityConfig {
         return authProvider;
     }
 
+    // Creates an instance of the AuthenticationManager bean.
     @Bean
     public AuthenticationManager authenticationManager( AuthenticationConfiguration authConfig ) throws Exception {
         return authConfig.getAuthenticationManager();
     }
 
+    // Creates an instance of the BCryptPasswordEncoder bean.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // Configures the security filter chain.
     @Bean
     public SecurityFilterChain filterChain( HttpSecurity http, HandlerMappingIntrospector introspector ) throws Exception {
         MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder( introspector );
